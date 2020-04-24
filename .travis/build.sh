e2ed302cec080fa03938df4267cb7b876c4a6f27
begotxe <begona@gridscale.io>
2020-04-24 16:05:51 +0200

    check if the test works from the build stage

--- .travis/build.sh
+++ .travis/build.sh
@@ -5,3 +5,9 @@ echo "Building version $(python setup.py --version)"
 python setup.py sdist bdist_wheel
 ls -a
 echo "Building Done"
+
+echo "... Testing sdist package ..."
+
+pip install dist/gs_api_client-$(python setup.py --version).tar.gz
+
+echo "... Testing sdist package done ..."
--- .travis/test_sdist_package.sh
+++ .travis/test_sdist_package.sh
@@ -2,8 +2,7 @@
 set -ev
 
 echo "... Testing sdist package ..."
-cd dist
-ls -a
-pip install ./gs_api_client-$(python setup.py --version).tar.gz
+
+pip install dist/gs_api_client-$(python setup.py --version).tar.gz
 
 echo "... Testing sdist package done ..."
