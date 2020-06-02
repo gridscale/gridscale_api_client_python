374dd297d3847fb47a35d8ba2c720f0afcc94845
begotxe <begona@gridscale.io>
2020-06-02 11:54:21 +0200

    wip

--- .travis/test_sdist_wheel_package.sh
+++ .travis/test_sdist_wheel_package.sh
@@ -2,6 +2,6 @@
 
 echo "Testing sdist wheel package $(python setup.py --version)"
 
-pip install dist/gs_api_client-$(python setup.py --version).tar.gz
+pip install dist/gs_api_client-$(python setup.py --version)-py3-none-any.whl
 
 echo "Testing sdist wheel package done"
