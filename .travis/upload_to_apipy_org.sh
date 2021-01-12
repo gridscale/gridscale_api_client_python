4c9eab4454471d480aa02ea0a1ab16658cb7e8bb
begotxe <begona@gridscale.io>
2021-01-12 16:40:13 +0100

    fix pip incompatibility

--- .travis/upload_to_apipy_org.sh
+++ .travis/upload_to_apipy_org.sh
@@ -5,7 +5,8 @@ echo "Uploading package to pypi.org"
 ls -l
 
 python setup.py sdist bdist_wheel
-pip install twine --use-feature=2020-resolver
+pip install --upgrade pip
+pip install twine
 twine upload -u __token__ -p "$PYPI_ORG_TOKEN" dist/gs_api_client-*
 
 echo "Upload Done"
