2c620621beae4eaa65a77500c68198e17bda6b53
Benjamin Kircher <bkircher@gridscale.io>
2022-04-22 11:12:10 +0200

    Add test case for load_config
    Add a test case that loads a simple YAML file with two projects. You can
    run tests by executing `pytest` in the project root. Note that the test
    currently fails.

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,10 @@
-from gs_api_client import Configuration
+import os.path
 
+from gs_api_client import Configuration
+from examples.configloader import load_config
+
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 def test_debug_is_disabled_by_default():
     config = Configuration()
@@ -9,3 +14,14 @@ def test_debug_is_disabled_by_default():
 def test_tls_certs_are_verified_by_default():
     config = Configuration()
     assert config.verify_ssl
+
+
+def test_load_config_from_yaml():
+    """"Make sure we can load a config from a given YAML file."""
+
+    example_config = os.path.join(CURRENT_DIR, "example-config.yaml")
+    res = load_config(example_config)
+    assert isinstance(res, list)
+    assert len(res) == 2
+    assert res[0]["name"] == "default"
+    assert res[1]["name"] == "something-else"
